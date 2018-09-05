259f9d28b6968060c6dc1ba2220ed9ad3791e0a5
IMRON REVIADY <imronreviady2@gmail.com>
2018-09-05 18:07:32 +0700

    create Home page and Create Quiz page

--- src/router.js
+++ src/router.js
@@ -1,12 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Home from '@/pages/Home'
+import Create from '@/pages/Create'
+
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/create',
+      name: 'Create',
+      component: Create
+    }
   ]
 })
