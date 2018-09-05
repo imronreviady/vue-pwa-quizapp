cd5d32a082d54ed70418f3aa67e3b87a94dfd258
IMRON REVIADY <imronreviady2@gmail.com>
2018-09-06 06:52:44 +0700

    add ability to remove answer and remove questions

---
+++ src/components/Quiz/QuizQuestions.vue
@@ -0,0 +1,97 @@
+<template>
+	<v-container fluid>
+		<v-layout wrap>
+			<v-flex xs12>
+				<v-expansion-panel focusable>
+					<v-expansion-panel-content
+					v-for="(item, questionIndex) in quiz.questions"
+					:key="questionIndex"
+					focusable
+					>
+						<div slot="header">{{item.question}}</div>
+						<v-card class="grey lighten-3">
+							<v-container>
+								<v-layout wrap>
+									<v-flex xs10>
+										<v-text-field
+										label="Question"
+										:value="item.question"
+										></v-text-field>
+									</v-flex>
+									<v-flex xs2>
+										<v-text-field
+										type="number"
+										label="Points"
+										:value="item.points"
+										></v-text-field>
+									</v-flex>
+								</v-layout>
+								<v-container>
+									<v-layout
+									wrap
+									v-for="(answer, answerIndex) in item.answers"
+									:key="answerIndex"
+									>
+										<v-flex xs10>
+											<v-text-field
+											label="Answer"
+											:value="answer.answer"
+											></v-text-field>
+										</v-flex>
+										<v-flex xs2>
+											<v-btn
+											fab 
+											dark 
+											small 
+											color="red"
+											@click="removeAnswer({questionIndex, answerIndex})"
+											>
+												<v-icon>remove</v-icon>
+											</v-btn>
+										</v-flex>
+									</v-layout>
+								</v-container>
+								<v-layout class="pl-2">
+									<v-btn
+									v-if="item.answers.length < 5"
+									class="mt-4"
+									color="primary"
+									@click="addAnswer(questionIndex)"
+									>Add Answer</v-btn>
+									<v-btn
+									class="mt-4"
+									color="red"
+									dark
+									@click="removeQuestion(questionIndex)"
+									>Remove Question</v-btn>
+								</v-layout>
+							</v-container>
+						</v-card>
+					</v-expansion-panel-content>
+				</v-expansion-panel>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+	import { mapMutations, mapGetters } from 'vuex'
+
+	import { ADD_ANSWER, REMOVE_QUESTION, REMOVE_ANSWER } from '@/store/quiz/mutations'
+
+	export default {
+		name: 'quiz-question',
+		computed: {
+			...mapGetters('quiz', {
+				quiz: 'newQuiz'
+			})
+		},
+		methods: {
+			...mapMutations('quiz', {
+				addAnswer: ADD_ANSWER,
+				removeQuestion: REMOVE_QUESTION,
+				removeAnswer: REMOVE_ANSWER
+			})
+		}
+	}
+</script>
--- src/store/quiz/index.js
+++ src/store/quiz/index.js
@@ -1,13 +1,53 @@
+import { UPDATE_INFORMATION, ADD_ANSWER, REMOVE_QUESTION, REMOVE_ANSWER } from './mutations'
+
 const state = {
-  quiz: 'a funny quiz'
+	newQuiz: {
+		title: "Quiz 2018",
+		description: "An awesome quiz!",
+		questions: [{
+			question: "First Question",
+			points: 50,
+			answers: [{
+				answer: "First answer",
+				isRight: false
+			}]
+		}]
+	}
 }
 
 const getters = {
-  
+	newQuiz: ({newQuiz}) => newQuiz
 }
 
 const mutations = {
+	[UPDATE_INFORMATION](state, info) {
+		state.newQuiz.title = info.title
+		state.newQuiz.description = info.description
+	},
+	[ADD_ANSWER](state, questionIndex) {
+		const answers = state.newQuiz.questions[questionIndex].answers;
+		if (answers.length < 5) {
+			answers.push({
+				answer: "Another one!",
+				isRight: false
+			})
+		}
+	},
+	[REMOVE_QUESTION](state, questionIndex) {
+		if (state.newQuiz.questions.length > 1) {
+			state.newQuiz.questions.splice(questionIndex, 1)
+		}
+	},
+	[REMOVE_ANSWER](state, payload) {
+		const questionIndex = payload.questionIndex
+		const answerIndex = payload.answerIndex
 
+		const question = state.newQuiz.questions[questionIndex]
+
+		if (question.answers.length > 1) {
+			question.answers.splice(answerIndex, 1)
+		}
+	}
 }
 
 const actions = {
