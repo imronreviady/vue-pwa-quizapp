31538acdf39551ae232865a52a4984011305e61b
IMRON REVIADY <imronreviady2@gmail.com>
2018-09-06 13:46:57 +0700

    Add quiz page and create route by taking quiz id from url

---
+++ src/pages/Quiz.vue
@@ -0,0 +1,28 @@
+<template>
+	<v-card>
+
+	</v-card>
+</template>
+
+<script>
+	import { mapGetter, mapActions } from 'vuex'
+
+	export default {
+		name: 'quiz',
+		created() {
+			// get the quiz using the id passed in url params
+			this.getQuiz(this.$route.params.id)
+		},
+		computed: {
+
+		},
+		methods: {
+			...mapActions('quiz', {getQuiz: 'get'})
+		},
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
